909242c6d9fa70bfa96dc820789b19c58773c1d0
maddie-houseman <HOUSEMM@wellingtoncollege.org.uk>
2025-10-15 15:18:16 +0100

    H

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: { extend: {} },
+    theme: { 
+        extend: {
+            fontFamily: {
+                'sans': ['Plus Jakarta Sans', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif'],
+            }
+        } 
+    },
     plugins: [],
 };
